27cd713860723211cb11c9bbb98d47ce510bc266
Dmitriy Sosnovich /Dmitrij Sosnovič/Дмитрий Соснович <dmitriy@sosnovich.com>
2020-12-26 18:54:32 +0100

    SonarLint Issues

--- src/main/java/cz/sosnovich/hlavalidator/hlavalidator/nmdp_web_service/client/invoker/CustomInstantDeserializer.java
+++ src/main/java/cz/sosnovich/hlavalidator/hlavalidator/nmdp_web_service/client/invoker/CustomInstantDeserializer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonTokenId;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.datatype.threetenbp.DecimalUtils;
 import com.fasterxml.jackson.datatype.threetenbp.deser.ThreeTenDateTimeDeserializerBase;
 import com.fasterxml.jackson.datatype.threetenbp.function.BiFunction;
@@ -25,144 +26,152 @@ import java.math.BigDecimal;
  */
 public class CustomInstantDeserializer<T extends Temporal>
         extends ThreeTenDateTimeDeserializerBase<T> {
-  public static final CustomInstantDeserializer<Instant> INSTANT = new CustomInstantDeserializer<>(
-          Instant.class, DateTimeFormatter.ISO_INSTANT,
-          Instant::from,
-          a -> Instant.ofEpochMilli(a.value),
-          a -> Instant.ofEpochSecond(a.integer, a.fraction),
-          null
-  );
-  public static final CustomInstantDeserializer<OffsetDateTime> OFFSET_DATE_TIME = new CustomInstantDeserializer<>(
-          OffsetDateTime.class, DateTimeFormatter.ISO_OFFSET_DATE_TIME,
-          OffsetDateTime::from,
-          a -> OffsetDateTime.ofInstant(Instant.ofEpochMilli(a.value), a.zoneId),
-          a -> OffsetDateTime.ofInstant(Instant.ofEpochSecond(a.integer, a.fraction), a.zoneId),
-          (d, z) -> d.withOffsetSameInstant(z.getRules().getOffset(d.toLocalDateTime()))
-  );
-  public static final CustomInstantDeserializer<ZonedDateTime> ZONED_DATE_TIME = new CustomInstantDeserializer<>(
-          ZonedDateTime.class, DateTimeFormatter.ISO_ZONED_DATE_TIME,
-          ZonedDateTime::from,
-          a -> ZonedDateTime.ofInstant(Instant.ofEpochMilli(a.value), a.zoneId),
-          a -> ZonedDateTime.ofInstant(Instant.ofEpochSecond(a.integer, a.fraction), a.zoneId),
-          ZonedDateTime::withZoneSameInstant
-  );
-  private static final long serialVersionUID = 1L;
-  protected final Function<FromIntegerArguments, T> fromMilliseconds;
+    public static final CustomInstantDeserializer<Instant> INSTANT = new CustomInstantDeserializer<>(
+            Instant.class, DateTimeFormatter.ISO_INSTANT,
+            Instant::from,
+            a -> Instant.ofEpochMilli(a.value),
+            a -> Instant.ofEpochSecond(a.integer, a.fraction),
+            null
+    );
+    public static final CustomInstantDeserializer<OffsetDateTime> OFFSET_DATE_TIME = new CustomInstantDeserializer<>(
+            OffsetDateTime.class, DateTimeFormatter.ISO_OFFSET_DATE_TIME,
+            OffsetDateTime::from,
+            a -> OffsetDateTime.ofInstant(Instant.ofEpochMilli(a.value), a.zoneId),
+            a -> OffsetDateTime.ofInstant(Instant.ofEpochSecond(a.integer, a.fraction), a.zoneId),
+            (d, z) -> d.withOffsetSameInstant(z.getRules().getOffset(d.toLocalDateTime()))
+    );
+    public static final CustomInstantDeserializer<ZonedDateTime> ZONED_DATE_TIME = new CustomInstantDeserializer<>(
+            ZonedDateTime.class, DateTimeFormatter.ISO_ZONED_DATE_TIME,
+            ZonedDateTime::from,
+            a -> ZonedDateTime.ofInstant(Instant.ofEpochMilli(a.value), a.zoneId),
+            a -> ZonedDateTime.ofInstant(Instant.ofEpochSecond(a.integer, a.fraction), a.zoneId),
+            ZonedDateTime::withZoneSameInstant
+    );
+    private static final long serialVersionUID = 1L;
 
-  protected final Function<FromDecimalArguments, T> fromNanoseconds;
+    protected final transient Function<FromIntegerArguments, T> fromMilliseconds;
 
-  protected final Function<TemporalAccessor, T> parsedToValue;
+    protected final transient Function<FromDecimalArguments, T> fromNanoseconds;
 
-  protected final BiFunction<T, ZoneId, T> adjust;
+    protected final transient Function<TemporalAccessor, T> parsedToValue;
 
-  protected CustomInstantDeserializer(Class<T> supportedType,
-                                      DateTimeFormatter parser,
-                                      Function<TemporalAccessor, T> parsedToValue,
-                                      Function<FromIntegerArguments, T> fromMilliseconds,
-                                      Function<FromDecimalArguments, T> fromNanoseconds,
-                                      BiFunction<T, ZoneId, T> adjust) {
-    super(supportedType, parser);
-    this.parsedToValue = parsedToValue;
-    this.fromMilliseconds = fromMilliseconds;
-    this.fromNanoseconds = fromNanoseconds;
-    this.adjust = adjust == null ? new BiFunction<T, ZoneId, T>() {
-      @Override
-      public T apply(T t, ZoneId zoneId) {
-        return t;
-      }
-    } : adjust;
-  }
+    protected final transient BiFunction<T, ZoneId, T> adjust;
 
-  @SuppressWarnings("unchecked")
-  protected CustomInstantDeserializer(CustomInstantDeserializer<T> base, DateTimeFormatter f) {
-    super((Class<T>) base.handledType(), f);
-    parsedToValue = base.parsedToValue;
-    fromMilliseconds = base.fromMilliseconds;
-    fromNanoseconds = base.fromNanoseconds;
-    adjust = base.adjust;
-  }
-
-  @Override
-  protected JsonDeserializer<T> withDateFormat(DateTimeFormatter dtf) {
-    if (dtf == _formatter) {
-      return this;
+    protected CustomInstantDeserializer(Class<T> supportedType,
+                                        DateTimeFormatter parser,
+                                        Function<TemporalAccessor, T> parsedToValue,
+                                        Function<FromIntegerArguments, T> fromMilliseconds,
+                                        Function<FromDecimalArguments, T> fromNanoseconds,
+                                        BiFunction<T, ZoneId, T> adjust) {
+        super(supportedType, parser);
+        this.parsedToValue = parsedToValue;
+        this.fromMilliseconds = fromMilliseconds;
+        this.fromNanoseconds = fromNanoseconds;
+        this.adjust = adjust == null ? (t, zoneId) -> t : adjust;
     }
-    return new CustomInstantDeserializer<>(this, dtf);
-  }
 
-  @Override
-  public T deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-    //NOTE: Timestamps contain no timezone info, and are always in configured TZ. Only
-    //string values have to be adjusted to the configured TZ.
-    switch (parser.getCurrentTokenId()) {
-      case JsonTokenId.ID_NUMBER_FLOAT: {
+    @SuppressWarnings("unchecked")
+    protected CustomInstantDeserializer(CustomInstantDeserializer<T> base, DateTimeFormatter f) {
+        super((Class<T>) base.handledType(), f);
+        parsedToValue = base.parsedToValue;
+        fromMilliseconds = base.fromMilliseconds;
+        fromNanoseconds = base.fromNanoseconds;
+        adjust = base.adjust;
+    }
+
+    @Override
+    protected JsonDeserializer<T> withDateFormat(DateTimeFormatter dtf) {
+        if (dtf == _formatter) {
+            return this;
+        }
+        return new CustomInstantDeserializer<>(this, dtf);
+    }
+
+    @Override
+    public T deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        //NOTE: Timestamps contain no timezone info, and are always in configured TZ. Only
+        //string values have to be adjusted to the configured TZ.
+        switch (parser.getCurrentTokenId()) {
+            case JsonTokenId.ID_NUMBER_FLOAT:
+                return getJsonTokenIdIdNumberFloat(parser, context);
+
+            case JsonTokenId.ID_NUMBER_INT:
+                return getJsonTokenIdIdNumberInt(parser, context);
+
+            case JsonTokenId.ID_STRING:
+                return getJsonTokenIdString(parser, context);
+            default:
+                throw JsonMappingException.from(parser, "Expected type float, integer, or string.");
+        }
+
+    }
+
+    private T getJsonTokenIdIdNumberFloat(JsonParser parser, DeserializationContext context) throws IOException {
         BigDecimal value = parser.getDecimalValue();
         long seconds = value.longValue();
         int nanoseconds = DecimalUtils.extractNanosecondDecimal(value, seconds);
         return fromNanoseconds.apply(new FromDecimalArguments(
                 seconds, nanoseconds, getZone(context)));
-      }
+    }
 
-      case JsonTokenId.ID_NUMBER_INT: {
+    private T getJsonTokenIdIdNumberInt(JsonParser parser, DeserializationContext context) throws IOException {
         long timestamp = parser.getLongValue();
         if (context.isEnabled(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS)) {
-          return this.fromNanoseconds.apply(new FromDecimalArguments(
-                  timestamp, 0, this.getZone(context)
-          ));
+            return this.fromNanoseconds.apply(new FromDecimalArguments(
+                    timestamp, 0, this.getZone(context)
+            ));
         }
         return this.fromMilliseconds.apply(new FromIntegerArguments(
                 timestamp, this.getZone(context)
         ));
-      }
+    }
 
-      case JsonTokenId.ID_STRING: {
+
+    private T getJsonTokenIdString(JsonParser parser, DeserializationContext context) throws IOException {
         String string = parser.getText().trim();
         if (string.length() == 0) {
-          return null;
+            return null;
         }
         if (string.endsWith("+0000")) {
-          string = string.substring(0, string.length() - 5) + "Z";
+            string = string.substring(0, string.length() - 5) + "Z";
         }
         T value;
         try {
-          TemporalAccessor acc = _formatter.parse(string);
-          value = parsedToValue.apply(acc);
-          if (context.isEnabled(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)) {
-            return adjust.apply(value, this.getZone(context));
-          }
+            TemporalAccessor acc = _formatter.parse(string);
+            value = parsedToValue.apply(acc);
+            if (context.isEnabled(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)) {
+                return adjust.apply(value, this.getZone(context));
+            }
         } catch (DateTimeException e) {
-          throw _peelDTE(e);
+            throw _peelDTE(e);
         }
         return value;
-      }
     }
-    throw context.mappingException("Expected type float, integer, or string.");
-  }
 
-  private ZoneId getZone(DeserializationContext context) {
-    // Instants are always in UTC, so don't waste compute cycles
-    return (_valueClass == Instant.class) ? null : DateTimeUtils.toZoneId(context.getTimeZone());
-  }
-
-  private static class FromIntegerArguments {
-    public final long value;
-    public final ZoneId zoneId;
-
-    private FromIntegerArguments(long value, ZoneId zoneId) {
-      this.value = value;
-      this.zoneId = zoneId;
+    private ZoneId getZone(DeserializationContext context) {
+        // Instants are always in UTC, so don't waste compute cycles
+        return (_valueClass == Instant.class) ? null : DateTimeUtils.toZoneId(context.getTimeZone());
     }
-  }
 
-  private static class FromDecimalArguments {
-    public final long integer;
-    public final int fraction;
-    public final ZoneId zoneId;
+    private static class FromIntegerArguments {
+        public final long value;
+        public final ZoneId zoneId;
 
-    private FromDecimalArguments(long integer, int fraction, ZoneId zoneId) {
-      this.integer = integer;
-      this.fraction = fraction;
-      this.zoneId = zoneId;
+        private FromIntegerArguments(long value, ZoneId zoneId) {
+            this.value = value;
+            this.zoneId = zoneId;
+        }
     }
-  }
+
+    private static class FromDecimalArguments {
+        public final long integer;
+        public final int fraction;
+        public final ZoneId zoneId;
+
+        private FromDecimalArguments(long integer, int fraction, ZoneId zoneId) {
+            this.integer = integer;
+            this.fraction = fraction;
+            this.zoneId = zoneId;
+        }
+    }
 }
